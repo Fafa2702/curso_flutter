2e457e3e1f0e37ac648a3a28410d3ecaa928e332
Ana Flavia Lobato <22400656@aluno.cotemig.com.br>
2025-04-27 15:58:44 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/tela_inicial.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:const InitialScreen() // Coloque const aqui também pra melhorar
+    );
+  }
+}
+
+
+
+
+
---
+++ lib/screen/tela_inicial.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/tasks.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacidade = true; // <-- variável antes do @override
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter: Primeiros Passos'),
+        leading: const Icon(Icons.add_task),
+      ),
+      body: AnimatedOpacity(
+        opacity: opacidade ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 500),
+        child: Container(
+          color: const Color.fromARGB(255, 208, 221, 237),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: Tasks('Estudar Flutter', ' ', 3),
+              ),
+              Tasks('Andar de Bike', 'assets/images/bike.webp', 2),
+              Tasks('Ler 50 páginas', 'assets/images/ler.jpg', 1),
+              Tasks('Meditar', 'assets/images/meditar.jpeg', 4),
+              Tasks('Jogar', 'assets/images/jogar.jpg', 0),
+              SizedBox(height: 100),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            opacidade = !opacidade;
+          });
+        },
+        backgroundColor: Colors.blue[100],
+        child: const Icon(Icons.remove_red_eye),
+      ),
+    );
+  }
+}
