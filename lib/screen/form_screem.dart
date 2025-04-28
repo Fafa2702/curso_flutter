bfa3819db68598ab85c4b78cafa4bc228121acfd
Ana Flavia Lobato <22400656@aluno.cotemig.com.br>
2025-04-28 18:19:33 -0300

    Update form_screem.dart

--- lib/screen/form_screem.dart
+++ lib/screen/form_screem.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget { // Corrigido Screem -> Screen
+  const FormScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nova Tarefa"),
+      ),
+      body: Center(
+        child: Container(
+          height: 650,
+          width: 375,
+          decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 3),
+
+          ),
+          child: Column(
+            children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: ,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Nome',
+                  fillColor: Colors.white30,
+                  filled: true,
+              
+                ),
+              ),
+            )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
